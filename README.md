6cbeba6be4ca2c61ecb81be911660535d01fc29f
thomaslienbacher <lienbacher.tom@gmail.com>
2019-11-29 19:47:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# net-search
+
+### Schulprojekt
+
+Authoren:
+
+* Thomas Lienbacher 
+* Elvir Hodzic 
+* Noa Hauswirth 
+* Stevan Vukovic
+
+Anforderungen:
+
+* PHP 7.4
+* OpenJDK 13
---
+++ nese-rest-api/src/main/java/swp/netsearch/restapi/Main.java
@@ -0,0 +1,21 @@
+package swp.netsearch.restapi;
+
+import org.restlet.Component;
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+public class Main extends ServerResource {
+    public static void main(String[] args) throws Exception {
+        Component component = new Component();
+        component.getServers().add(Protocol.HTTP, 8100);
+        component.getDefaultHost().attach("/v1", Main.class);
+        component.start();
+    }
+
+    @Get
+    public String rand() {
+        return "rand: " + Math.random();
+    }
+}
